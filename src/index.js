b4a706f68aa2099676d03506d9e43139ac6f5414
Romashkina <da_shootka@mail.ru>
2022-01-10 22:08:59 +0100

    implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let i = 0;
+  let result = '';
+
+  while (i < str.length) {
+    str[i] !== '-' ? result = `${str[i]}${result}` : result = `${result}`;
+      i++;
+  }
+  return result * 1;
 }
